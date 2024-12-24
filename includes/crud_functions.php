43ba0b54905b8dfc64d8ae42ecf95ad8a45d6296
Salma-007 <salmaelallali11@gmail.com>
2024-12-25 00:21:51 +0100

    dashboard completed with no errors

--- includes/crud_functions.php
+++ includes/crud_functions.php
@@ -34,16 +34,42 @@ function get_all_articles($mysqli){
 
 
 function get_category_stats($mysqli){
+    $query = "SELECT COUNT(*) as article_count, categories.name as category_name FROM articles JOIN categories ON articles.category_id = categories.id GROUP BY category_name";
+    $result = mysqli_query($mysqli,$query);
+    if($result){
+        $category_stats = [];
+        while($row = mysqli_fetch_assoc($result)){
+            $category_stats[] = $row;
+        }
+        return $category_stats;
+    }
 
 }
 
 function get_top_users($mysqli){
-    $query = "select * from users ";
-
+    $query = "select users.id as id, username, count(articles.id) as article_count, articles.views as total_views from users join articles on users.id = articles.author_id order by article_count limit 3;";
+    $result = mysqli_query($mysqli,$query);
+    if($result){
+        $users = [];
+        while($row = mysqli_fetch_assoc($result)){
+            $users[] = $row;
+        }
+        return $users;
+    }
 
 }
 
 function get_top_articles($mysqli){
+    $query = "select articles.id, created_at, title, users.username as author_name, views from articles join users on articles.author_id = users.id order by views limit 3;";
+    $result = mysqli_query($mysqli,$query);
+    if($result){
+        $top_articles = [];
+        while($row = mysqli_fetch_assoc($result)){
+            $top_articles[] = $row;
+        }
+        return $top_articles;
+
+    }
     
 }
 
